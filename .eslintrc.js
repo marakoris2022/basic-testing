f2a1332505e4cc0fc7258a58bfc888bdee7b47f0
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-10-18 23:18:42 +0300

    feat: prettier correction

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,10 +21,4 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
-  'prettier/prettier': [
-    'error',
-    {
-      endOfLine: 'auto',
-    },
-  ],
 };
