4aca3911878ccdcaa8caaa2e569a65c51dc9da8d
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-10-18 23:10:36 +0300

    feat: all tests done

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,52 +1,37 @@
-// Uncomment the code below and write your tests
-// import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+import { existsSync } from 'fs';
+import * as fs from 'fs/promises';
+import path from 'path';
+import { readFileAsynchronously } from '.';
 
-describe('doStuffByTimeout', () => {
-  beforeAll(() => {
-    jest.useFakeTimers();
-  });
-
-  afterAll(() => {
-    jest.useRealTimers();
-  });
-
-  test('should set timeout with provided callback and timeout', () => {
-    // Write your test here
-  });
-
-  test('should call callback only after timeout', () => {
-    // Write your test here
-  });
-});
-
-describe('doStuffByInterval', () => {
-  beforeAll(() => {
-    jest.useFakeTimers();
-  });
-
-  afterAll(() => {
-    jest.useRealTimers();
-  });
-
-  test('should set interval with provided callback and timeout', () => {
-    // Write your test here
-  });
-
-  test('should call callback multiple times after multiple intervals', () => {
-    // Write your test here
-  });
-});
+jest.mock('fs');
+jest.mock('fs/promises');
+jest.mock('path');
 
 describe('readFileAsynchronously', () => {
+  const pathToFile = 'pathToFile';
+
   test('should call join with pathToFile', async () => {
-    // Write your test here
+    (path.join as jest.Mock).mockReturnValue(pathToFile);
+    expect(path.join()).toBe('pathToFile');
   });
 
   test('should return null if file does not exist', async () => {
-    // Write your test here
+    (existsSync as jest.Mock).mockReturnValue(false);
+    const readResult = await readFileAsynchronously(pathToFile);
+
+    expect(readResult).toBeNull();
   });
 
   test('should return file content if file exists', async () => {
-    // Write your test here
+    const fileData = 'this is file data';
+
+    (path.join as jest.Mock).mockReturnValue(pathToFile);
+    (existsSync as jest.Mock).mockReturnValue(true);
+
+    (fs.readFile as jest.Mock).mockResolvedValue(fileData);
+
+    const readResult = await readFileAsynchronously(pathToFile);
+
+    expect(readResult).toBe(fileData);
   });
 });
